1ddadaae0652980299e6fd8ccc97f5d62d206fa0
lgridsaffan <shaffanabrorr6s@gmail.com>
2021-06-01 12:48:34 +0700

    ubah navbar

--- template/header.php
+++ template/header.php
@@ -16,7 +16,9 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">App Inventaris</a>
+                <a class="navbar-brand" href="<?= base_url(); ?>/page/index.php?laman=beranda">
+                    <img src="<?= base_url(); ?>" alt="" width="40" height="40">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
