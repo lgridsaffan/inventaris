9c427d4f02906bad549a65871c4a4cfc287e647a
lgridsaffan <shaffanabrorr6s@gmail.com>
2021-06-01 12:50:37 +0700

    navbar brand

--- template/header.php
+++ template/header.php
@@ -17,7 +17,7 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="<?= base_url(); ?>/page/index.php?laman=beranda">
-                    <img src="<?= base_url(); ?>" alt="" width="40" height="40">
+                    <img src="<?= base_url(); ?>/page/img/sma1-logo.png" alt="" width="40" height="40">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
